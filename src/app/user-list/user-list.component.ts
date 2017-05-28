91637c5411881f169ad0e7e5be251da5eea1e15f
vladimir <vladimirkuzmych@gmail.com>
2017-05-28 23:56:12 +0300

    third commit

---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+
+  private names: string[];
+
+  constructor() {
+    this.names = ['Ari', 'Carlos', 'Felipe', 'Nate'];
+  }
+
+  ngOnInit() {
+  }
+
+}
+
